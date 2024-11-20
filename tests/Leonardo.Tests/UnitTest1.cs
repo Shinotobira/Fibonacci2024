b23e31e4a7eaf21ceb06d059338cea67973e74ca
Shinotobira <62753305+Shinotobira@users.noreply.github.com>
2024-11-20 15:27:40 +0100

    first commit

---
+++ tests/Leonardo.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace Leonardo.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var results = await Fibonacci.RunAsync(new string[] { "42" });
+        Assert.Equal(42, results[0].Input);
+        Assert.Equal(267914296, results[0].Result);
+    }
+}
